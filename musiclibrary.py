c015db08f259e886bdc51c48f2675ebbe748fe7f
Piyush_Nihalani <123048298+PiyushNihalani@users.noreply.github.com>
2024-06-27 02:28:57 +0530

    Update musiclibrary.py

--- musiclibrary.py
+++ musiclibrary.py
@@ -1,6 +1,6 @@
 music = {
     "Lonely": "https://music.youtube.com/watch?v=6EEW-9NDM5k",
-    "song-name": "song-link",
+    "song-name": "song-link",#change to song name you want and add link of yt to listen
     "song-name": "song-link",
     "song-name": "song-link"
 }
