d4733bb566e255ffd71799619cf299f7ea449369
Piyush_Nihalani <123048298+PiyushNihalani@users.noreply.github.com>
2024-06-27 02:14:24 +0530

    Update musiclibrary.py

--- musiclibrary.py
+++ musiclibrary.py
@@ -1,6 +1,6 @@
 music = {
-    "Lonely": "",
-    "KunFayaKun": "",
-    "Khamoshiyan": "",
-    "TumHo": ""
+    "Lonely": "https://music.youtube.com/watch?v=6EEW-9NDM5k",
+    "song-name": "song-link",
+    "song-name": "song-link",
+    "song-name": "song-link"
 }
